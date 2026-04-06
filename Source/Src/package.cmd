d3a14e00bec3633c7b040adf76dc6b186533197d
Kevin Zehrer <103638980+kzdev-net@users.noreply.github.com>
2026-04-05 22:18:51 -0500

    Add NuGet packaging metadata and assets for PrimeTime
    Improved NuGet packaging for KZDev.PrimeTime and KZDev.SystemClock.PrimeTime:
    - Added detailed NuGet metadata (title, tags, icon, readme, license, description, release notes, repo URL) to .csproj files
    - Included nuget.icon.png and nuget.readme.md in new Package folder and referenced in projects
    - Added package.cmd script for automated packing
    - Updated default version to 0.0.2 in Directory.Build.props
    - Upgraded Microsoft.Extensions.Configuration/Hosting to 10.0.5
    - Refactored project files for clearer packaging properties
    - Updated solution file to include Package folder and assets

---
+++ Source/Src/package.cmd
@@ -0,0 +1,19 @@
+﻿@ECHO OFF
+SETLOCAL
+SET SOLUTIONPATH=%~dp0..
+SET PROJECTPATH=%SOLUTIONPATH%Src\KZDev.PrimeTime
+
+PUSHD %SOLUTIONPATH%
+
+dotnet clean -c Release
+dotnet restore
+
+PUSHD %PROJECTPATH%
+ 
+dotnet pack -c Release -p:IsPacking=true -p:ContinuousIntegrationBuild=true
+
+POPD
+POPD
+
+:EOF
+ENDLOCAL
