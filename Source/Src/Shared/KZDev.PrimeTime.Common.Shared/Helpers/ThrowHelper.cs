1b9808a15ea11fd11cfd7ee4027eafab4ff49f48
Kevin Zehrer <103638980+kzdev-net@users.noreply.github.com>
2026-06-03 21:07:01 -0500

    Add ThrowHelper class for centralized exception handling
    Introduced Helpers\ThrowHelper.cs as an internal static partial class to centralize throwing of InvalidOperation and Argument exceptions using ProductionStrings messages. Included the new file in the project items and marked it with [ExcludeFromCodeCoverage]. The class currently contains placeholders for future error handling methods.

---
+++ Source/Src/Shared/KZDev.PrimeTime.Common.Shared/Helpers/ThrowHelper.cs
@@ -0,0 +1,23 @@
+// Copyright (c) Kevin Zehrer
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace KZDev.PrimeTime.Helpers;
+
+//################################################################################
+/// <summary>
+///   Centralizes throwing common invalid-operation and argument exceptions using
+///   messages from <see cref="KZDev.PrimeTime.Resources.ProductionStrings"/>.
+/// </summary>
+[ExcludeFromCodeCoverage]
+internal static partial class ThrowHelper
+{
+    #region InvalidOperation Errors
+
+    #endregion
+
+    #region Argument Errors
+
+    #endregion
+}
